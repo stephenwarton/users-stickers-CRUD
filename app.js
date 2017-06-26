af7d5a6bb8ffebc7ae69a8295644ef4172f424c5
CJ R <cj@null.computer>
2017-06-26 11:18:56 -0600

    :sparkles: Add JWT Auth

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: 'http://localhost:8080',
   credentials: true
 }));
 
--- auth/middleware.js
+++ auth/middleware.js
@@ -1,7 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
 function ensureLoggedIn(req, res, next){
-  console.log(req.signedCookies);
-  if(req.signedCookies.user_id){
-    next();
+  const authHeader = req.get('Authorization');
+  // console.log(req.signedCookies);
+  console.log(authHeader);
+  const token = authHeader.split(' ')[1];
+  if(token){
+    // verify token
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+      console.log('here', err, decoded);
+      if(!err) return next();
+      res.status(401)
+      next(new Error('Invalid token'))
+    });
   }else{
     res.status(401)
     next(new Error('Un-Authorized'))
@@ -9,8 +22,18 @@ function ensureLoggedIn(req, res, next){
 }
 
 function allowAccess(req, res, next){
-  if(req.signedCookies.user_id == req.params.id){
-    next();
+  const authHeader = req.get('Authorization');
+  // console.log(req.signedCookies);
+  console.log(authHeader);
+  const token = authHeader.split(' ')[1];
+  if(token){
+    // verify token
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+      console.log('here', err, decoded);
+      if(!err && req.params.id == decoded.id) return next();
+      res.status(401)
+      next(new Error('Un-Authorized'))
+    });
   }else{
     res.status(401)
     next(new Error('Un-Authorized'))
